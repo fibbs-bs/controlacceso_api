8267b38b3b08c6ac2ec69d27a0140ffe37689599
fibb-bs_Git <jhonatanalvarez99@outlook.com>
2022-06-22 15:57:19 -0400

    web

--- src/middleware/TongoyAPI.py
+++ src/middleware/TongoyAPI.py
@@ -4,10 +4,13 @@ def get():
     try:
         url = 'https://losvilos.ucn.cl/tongoy/ab.php?o=pa'
         x = requests.get(url)
-        if x.status_code==200:
+        json = x.json()
+        if len(json)>1:
             print("Tongoy API status:",x.status_code)
-            return x.json()
-        else: 
+            return json
+        else:
             return None
     except:
         return None
+
+print(get())
