df32d914d234beaccbd26cc406260f45cc3460da
Sandoval2345 <tomas.sandoval@alumnos.ucn.cl>
2022-07-11 16:12:15 -0400

    VistaControlAccesoLista
    Falta funcionalidad, esta esta en desarrollo, falta asignarle la funcionalidad a los botones dar y denegar acceso

--- src-web/frontend/src/pages/ControlAcceso/index.js
+++ src-web/frontend/src/pages/ControlAcceso/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Grid, Container, Paper, Avatar, Typography, Button, TextField} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import {  LockOutlined as LockOutlinedIcon } from '@material-ui/icons'
+import {  LockOutlined as LockOutlinedIcon} from '@material-ui/icons'
 import axios from 'axios';
-import { Link, useHistory} from 'react-router-dom'
-import { ListItemText } from '@mui/material'
+
+import { Form, } from 'react-bootstrap';
+
 
 const useStyles = makeStyles(theme=>({
     root:{
@@ -33,20 +34,23 @@ const useStyles = makeStyles(theme=>({
 
     },
     avatar:{
-        margin: theme.spacing(6),
+        margin: theme.spacing(4),
         marginBottom: theme.spacing(1),
-        backgroundColor: theme.palette.primary.main
+        backgroundColor: theme.palette.secondary.main
        
     },
     form:{
         width: '100%',
         marginTop: theme.spacing(1)
     },
-    button:{
-        margin: theme.spacing(3, 0, 2) //mrgen general
-        
+    button1:{
+        margin: theme.spacing(3, 0, 2), //mrgen general
+        backgroundColor: 'green'
     },
-
+    button2:{
+        margin: theme.spacing(3, 0, 2), //mrgen general
+        backgroundColor: 'red'
+    },
     text:{
         marginLeft: 155,
         marginTop: 30
@@ -61,26 +65,120 @@ const useStyles = makeStyles(theme=>({
 
 export default function ControlAcceso(){
     const classes = useStyles();
+    const [salas, setSalas] = useState([])
+    const [bloques, setBloques] = useState([])
+    const [ruts, setRuts] = useState([])
+    const [salaSeleccionada, setSalaSeleccionada] = useState(null)
+    const [rutSeleccionado, setRutSeleccionado] = useState(null)
+    const [bloqueSeleccionado, setBloqueSeleccionado] = useState(null)
+
+    const getSalas = async () =>{
+        await axios.get('http://localhost:4000/api/getSalas')
+        .then(response =>{
+            //console.log(response)
+            setSalas(response.data)
+        })
+        .catch((e) => console.log(e))
+    }
+
+    const getBloques = async () =>{
+        await axios.get('http://localhost:4000/api/getBloques')
+        .then(response =>{
+            setBloques(response.data)
+        })
+        .catch((e) => console.log(e))
+    }
+
+    const getRuts = async () =>{
+        await axios.get('http://localhost:4000/api/getRuts')
+        .then(response =>{
+            setRuts(response.data)
+        })
+        .catch((e) => console.log(e))
+    }
+    
+    const generarAcceso = async () =>{
+        await (await axios.post ('http://localhost:4000/api/darAcceso', {sala: salaSeleccionada, bloque: bloqueSeleccionado, rut: rutSeleccionado}))
+        .then(response =>{
+
+        })
+    }
+
+    const changeSalaSeleccionada= (e) =>{
+        const value = e.target.value;
+        setSalaSeleccionada(value)
+    }
+    const changeBloqueSeleccionado = (e) =>{
+        const value = e.target.value;
+        setBloqueSeleccionado(value)
+    }
+    const changeRutSeleccionado = (e) => {
+        const value = e.target.value;
+        setRutSeleccionado(value)
+    }
+
+    useEffect (() => {
+
+        getSalas()
+        getBloques()
+        getRuts()
+    },[])
+    console.log({codigo_sala: salaSeleccionada})
+    console.log({bloque:bloqueSeleccionado})
+    console.log({rut:rutSeleccionado})
     return(
         <div>
             <Grid container component ='main' className={classes.root}>
                 <Container component={Paper} elevation={5} maxWidth = 'xs' className = {classes.container}>
                     <div className = {classes.div} align = 'center'> 
+
+                         <Avatar className = {classes.avatar} sx={{ width: 56, height: 56 }} > 
+                            <LockOutlinedIcon / /*ICONO DE CANDADO*/> 
+                        </Avatar>
+                        <br></br><br></br>
+                        <Typography component = 'h1' variant = 'h4'> Administrar Accesos </Typography>
+                        <br></br>
                         <form className = {classes.form} >
-                            <div className='form-group'>
-                                <select className='form-control'>
-                                    <option>Salas</option>
-                                </select>
-                                <br></br><br></br>
-                                <select>
-                                    <option>Bloque</option>
-                                </select>
-                            </div>
+                            <Form.Control column sm = {15} as = 'select' onClick = {getSalas} onChange = {changeSalaSeleccionada}>
+                                <option value ="" hidden > Salas </option>
+                                {salas.data?.map(sala => (
+                                    <option key = {sala.sala} value = {sala.sala}>{sala.nombre_sala}</option>
+                                    
+                                ))}
+                            </Form.Control>
+                            <br></br><br></br>
+                            <Form.Control column sm = {15} as = 'select' onClick = {getBloques} size = "lg" onChange = {changeBloqueSeleccionado}>
+                                <option value ="" hidden> Bloques </option>      
+                                {bloques.data?.map(bloque =>(
+                                    <option key = {bloque.bloque} value = {bloque.bloque}> {bloque.bloque} </option>
+                                ))}        
+                            </Form.Control>
+                            <br></br><br></br>
+                            <Form.Control column sm = {15} as = 'select' onClick = {getRuts} onChange = {changeRutSeleccionado}>
+                                <option value ="" hidden> Ruts </option>
+                                {ruts.data?.map(rut => (
+                                    <option key = {rut.rut} value = {rut.rut}> {rut.rut} </option>
+                                ))}
+                            </Form.Control>
+                            <br></br><br></br><br></br><br></br>
                             
                         </form>
+                        <Grid container>
+                            <Grid item xs = {6}>
+                                <Button align = 'center' className={classes.button1} size ='large'>Aprobar acceso</Button>
+                            </Grid>
+                            <Grid item xs = {6}> 
+                                <Button align = 'center' className={classes.button2} size ='large'>Denegar acceso</Button>
+                            </Grid>
+                        </Grid>    
                     </div>
                 </Container>
+                <br></br><br></br>
+                
+                
             </Grid>
+          
+            
         </div>
     )
 }
